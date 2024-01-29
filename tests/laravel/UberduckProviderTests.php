a3d0f3f7e45ff853bc434ec9a22673f735784881
Littlefish <berman82312@gmail.com>
2024-01-29 15:23:34 +0800

    Feat: add service provider tests

---
+++ tests/laravel/UberduckProviderTests.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace littlefish\Uberduck\laravel;
+
+use Illuminate\Support\Facades\Http;
+use littlefish\Uberduck\Uberduck;
+use littlefish\Uberduck\UberduckServiceProvider;
+use Orchestra\Testbench\TestCase;
+
+class UberduckProviderTests extends TestCase
+{
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            UberduckServiceProvider::class,
+        ];
+    }
+
+    public function test_uberduck_service_registered_correctly()
+    {
+        $uberduck = $this->app->make(Uberduck::class);
+
+        $this->assertInstanceOf(Uberduck::class, $uberduck);
+    }
+}
